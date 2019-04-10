6883939925710a255d6db0debbb026dfc2b7f0a1
肖绵合 <295082486@qq.com>
2019-04-11 01:06:57 +0800

    add cmd

--- config.sh
+++ config.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-if [ -f ~/.vimrc ]; then
-	cp ~/.vimrc .vimrc.old
-	rm ~/.vimrc 
-fi
-ln -s `pwd`/vimrc ~/.vimrc	
+# if [ -f ~/.vimrc ]; then
+# 	cp ~/.vimrc .vimrc.old
+# 	rm ~/.vimrc 
+# fi
+# ln -s `pwd`/vimrc ~/.vimrc	
+
+scr_path=`pwd`/scripts/
+grep -wq $scr_path ~/.bashrc && echo "scripts path is already configured" || echo "PATH=$scr_path:\$PATH" >> ~/.bashrc ; . ~/.bashrc
+
 
