921dcdea78a2661c4ceb11853b2f2014f7bed3d6
肖绵合 <295082486@qq.com>
2019-04-11 12:22:40 +0800

    1. add info error! please check and rm `pwd`/.vinrc.old 2. add info vimrc installation failed

--- config.sh
+++ config.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# if [ -f ~/.vimrc ]; then
-# 	cp ~/.vimrc .vimrc.old
-# 	rm ~/.vimrc 
-# fi
-# ln -s `pwd`/vimrc ~/.vimrc	
+if [ -f ~/.vimrc ]; then
+	if [ -f ./.vimrc.old ]; then
+		echo error! please check and rm `pwd`/.vinrc.old
+		echo vimrc installation failed
+	else
+		cp ~/.vimrc .vimrc.old
+		rm ~/.vimrc 
+		ln -s `pwd`/vimrc ~/.vimrc	
+	fi
+fi
 
 scr_path=`pwd`/scripts/
 grep -wq $scr_path ~/.bashrc && echo "scripts path is already configured" || echo "PATH=$scr_path:\$PATH" >> ~/.bashrc ; . ~/.bashrc
