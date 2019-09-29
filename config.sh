9eaa8e3feda2e681847338845fcdf4d72cacf141
masw <masw@masw.tech>
2019-09-29 06:23:44 -0700

    add bashrc

--- config.sh
+++ config.sh
@@ -12,9 +12,19 @@ else
 	ln -s `pwd`/vim/vimrc ~/.vimrc	
 fi
 
+
+# vim
+if [ -f ~/.vim/colors ]; then
+	cp ./vim/colors/space-vim-dark.vim ~/.vim/colors/
+else 
+	mkdir -p ~/.vim/colors
+	cp ./vim/colors/space-vim-dark.vim ~/.vim/colors/
+fi
+
+# bashrc 
 scr_path=`pwd`/scripts/
 grep -wq $scr_path ~/.bashrc && echo "scripts path is already configured" || echo "PATH=$scr_path:\$PATH" >> ~/.bashrc ; . ~/.bashrc
-
-cd vim 
-cp -rf ./color/ ~/.vim/
+bash_path=`pwd`
+stc="if [ -f $bash_path/bashrc ]; then\n\tsource $bash_path/bashrc\nfi\n"
+grep -wq $bash_path/bashrc ~/.bashrc && echo "bash path is already configured" || echo -e $stc >> ~/.bashrc 
 
